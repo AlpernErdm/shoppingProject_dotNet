753dff486e60110ca0c59cac4591e4c721a2d2d3
Alperen Erdem <alpernerdm@gmail.com>
2023-10-13 16:23:35 +0300

    Backend kodlanmaya başlandı

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,29 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal; //Bir iş sınıfı başka sınıfları newleyemez
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GettAll()
+        {
+            //İşKodları
+            return _productDal.GetAll();
+           
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,59 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal //Bellekte sanki datamız varmış gibi çalışacağız
+    {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            _products = new List<Product> {
+                new Product{CategoryID=1,ProductID=1,ProductName="Bardak",UnitPrice=15,UnitInStock=15 },
+                new Product{CategoryID=2,ProductID=1,ProductName="Kamera",UnitPrice=500,UnitInStock=3 },
+                new Product{CategoryID=3,ProductID=2,ProductName="Telefon",UnitPrice=1500,UnitInStock=2 },
+                new Product{CategoryID=4,ProductID=2,ProductName="Klavye",UnitPrice=150,UnitInStock=65 },
+                new Product{CategoryID=5,ProductID=2,ProductName="Fare",UnitPrice=85,UnitInStock=2 }
+            }; //Uygulama her çalıştığında listeye yeni eleman ekleyecek Constructor ile
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product); //Parametre olarak businessdan gönderilen ürünü veritabanına ekliyoruz  
+        }
+
+        public void Delete(Product product)
+        {
+            //LINQ=Language Integrated Query
+            Product productToDelete = _products.SingleOrDefault(p =>p.ProductID==product.ProductID); //linq sorgusu(foreachle aynı mantık)
+            _products.Remove(product);
+            //benım verdıgım p ile product'ın ıdsı benzer mı bak(döngü)
+            //p tek tek dolaşırken verdiğimiz takma isim
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            return _products.Where(p => p.CategoryID == categoryId).ToList();//where ile koşulumu sağlayan bütün ürünleri getirdim
+        }
+
+        public void Update(Product product)
+        {
+            //Gönderdiğim ürün id'sine sahip olan listedeki ürünü bul
+            Product productToUpdate = _products.SingleOrDefault(p => p.ProductID == product.ProductID);
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CategoryID = product.CategoryID;
+            productToUpdate.UnitInStock = product.UnitInStock;
+            productToUpdate.UnitPrice = product.UnitPrice;
+       
+        }
+    }
+}
